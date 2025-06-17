489c58170c8c878375bcb638952396b45b3f83eb
JochenWeerda <62930912+JochenWeerda@users.noreply.github.com>
2025-06-17 23:29:56 +0200

    Add skeleton custom modules

---
+++ custom_addons/valeo_batch_management/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'VALEO Chargenmanagement',
+    'version': '1.0',
+    'category': 'Inventory',
+    'summary': 'Chargen- und Rueckverfolgbarkeitsfunktionen',
+    'description': 'Grundlegendes Modul fuer Chargenverwaltung und Rueckverfolgbarkeit.',
+    'author': 'VALERO NeuroERP',
+    'website': 'https://www.valeo-neuroerp.com',
+    'depends': ['base', 'mail'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/batch_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ custom_addons/valeo_qs/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'VALEO QS Modul',
+    'version': '1.0',
+    'category': 'Quality',
+    'summary': 'Qualitaetsmanagement fuer VALEO NeuroERP',
+    'description': 'Basismodul fuer QS-Funktionen wie Pruefplan- und Reklamationsverwaltung.',
+    'author': 'VALERO NeuroERP',
+    'website': 'https://www.valeo-neuroerp.com',
+    'depends': ['base', 'mail'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/qs_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
